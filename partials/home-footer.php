e42214331546d7f4e64aa7903bfefeb108b17d01
Gerry Tucker <gerrytucker@gerrytucker.co.uk>
2015-03-06 03:03:48 +0000

    Auto-generated commit 06/03/2015 3:03:40 AM

--- partials/home-footer.php
+++ partials/home-footer.php
@@ -1,5 +1,7 @@
 <footer>
-	<div class="image"></div>
+	<div class="image">
+		<img src="<?php echo get_template_directory_uri(); ?>/images/footer.jpg">
+	</div>
 	<div class="footer row">
 		<div class="col l12">
 		</div>
