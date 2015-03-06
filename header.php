cedefbd4c9cfd51e958a519c95dedba5b3e529aa
Gerry Tucker <gerrytucker@gerrytucker.co.uk>
2015-03-06 00:39:03 +0000

    Auto-generated commit 06/03/2015 12:38:51 AM

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="<?php bloginfo('lang'); ?>">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no, minimal-ui"/>
+		<title><?php wp_title('|'); ?></title>
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-title" content="SPRFC">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+		<?php wp_head(); ?>
+	</head>
+	<body>
